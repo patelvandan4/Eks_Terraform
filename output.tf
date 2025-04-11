dfa8817839b26e272c05bc522d22a2f54e5b4c24
Vandan Patel <patelvandan029@gmail.com>
2025-04-11 15:16:31 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.patelvandan4.id
+  value = aws_eks_cluster.devopsshack.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.patelvandan4.id
+  value = aws_eks_node_group.devopsshack.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.patelvandan4_vpc.id
+  value = aws_vpc.devopsshack_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.patelvandan4_subnet[*].id
+  value = aws_subnet.devopsshack_subnet[*].id
 }
-
